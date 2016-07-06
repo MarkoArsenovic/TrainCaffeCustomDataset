d3171886f6da22e2ceab8663f4c2f89ff665c247
Rakesh Jasti <jas.rk11@gmail.com>
2016-07-06 18:42:03 +0530

    minor syntax errors

--- ImageNet/create_leveldb.sh
+++ ImageNet/create_leveldb.sh
@@ -8,12 +8,12 @@ DATA=/path/to/images/
 echo "Creating leveldb..."
 
 GLOG_logtostderr=1 $TOOLS/convert_imageset.bin -resize_width 256 -resize_height 256 -shuffle \
-    $DATA/ \
+    "" \
     $DATA/train.txt \
     train_leveldb
 
 GLOG_logtostderr=1 $TOOLS/convert_imageset.bin -resize_width 256 -resize_height 256 -shuffle \
-    $DATA/ \
+    "" \
     $DATA/val.txt \
     val_leveldb
     
